e38f7a9a9674ac63974191604e8fb29e3ea46a0d
EPifAnovVladIMIR <95338800+EPifAnovVladIMIR@users.noreply.github.com>
2021-12-15 00:55:44 +0300

    GRAPH 7
    GRAPH 7

---
+++ graph/graph 7.1/graph 7.1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace graph_7._1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+             
+            {
+               
+
+
+                string input = Convert.ToString(a.Text);
+                int ind = 0;
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (input.Substring(i, 1) != " ")
+                        ind++;
+                }
+                b.Text = Convert.ToString(ind);
+                
+            }
+        }
+    }
+}
