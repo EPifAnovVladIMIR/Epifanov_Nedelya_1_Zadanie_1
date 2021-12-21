bd3fd025c400b8cd59423535a90052d5867a496d
EPifAnovVladIMIR <95338800+EPifAnovVladIMIR@users.noreply.github.com>
2021-12-22 02:02:53 +0300

    asd
    asd

---
+++ console/ConsoleApp11/ConsoleApp11-c-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp11_c_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите номинал и количество купюр: ");
+                int first = int.Parse(Console.ReadLine());
+                int second = int.Parse(Console.ReadLine());
+
+                Money money = new Money(first, second);
+
+                Console.WriteLine(money);
+                Console.WriteLine($"Money summ: {money.MoneySumm}");
+                Console.WriteLine($"Money is enought to buy on 110: {money.IsEnought(110)}");
+                Console.WriteLine($"How much thing can buy on 113: {money.GetNumberOfProducts(113)}");
+            }
+            catch
+            {
+                // вывод сообщения об ошибке
+                Console.WriteLine("exception");
+            }
+        }
+    }
+}
